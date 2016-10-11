84b9cc6015ef35f327a32525c478173db04c78ea
yuyang <yuyang@qq.com>
2016-10-11 14:48:19 +0800

    add static method

--- Hello.java
+++ Hello.java
@@ -3,5 +3,10 @@ public class hello
 	public static void main(String args[]){
 		System.out.println("www.yuyang.com!!");
 		System.out.println("happy git !");
+		print();
+	}
+	public static void print(){
+		System.out.println("**************************");
+		System.out.println("hello world!");
 	}
 }
